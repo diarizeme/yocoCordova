ccef45b580f010e04e11d86de77d7f1d5ac3512e
Mark Raa <mark@diarize.me>
2020-02-26 06:55:01 +0200

    updated commit2

--- www/Yoco.js
+++ www/Yoco.js
@@ -3,7 +3,3 @@ var exec = require('cordova/exec');
 module.exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'Yoco', 'coolMethod', [arg0]);
 };
-
-module.exports.add = function (arg0, success, error) {
-    exec(success, error, 'Yoco', 'add', [arg0]);
-}
